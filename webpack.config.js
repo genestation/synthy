4aa3ccf6a87cafdd582f3a7d1ca73d6f66db63a9
Mara Kim <mara.kim@vanderbilt.edu>
2016-03-24 14:34:16 -0500

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const common = {
+	entry: {
+		app: PATHS.app
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	output: {
+		path: PATHS.build,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css'],
+				include: PATHS.app
+			},
+			{
+				test: /\.jsx?$/,
+				loaders: ['babel?cacheDirectory'],
+				include: PATHS.app
+			}
+		]
+	},
+};
