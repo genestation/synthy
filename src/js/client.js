80afbebfcced6eb24c209820ec44e515efb31856
Mara Kim <mara.kim@vanderbilt.edu>
2016-05-19 15:07:57 -0500

    Generate iframe programmatically

--- src/js/client.js
+++ src/js/client.js
@@ -1,3 +1,16 @@
 "use strict";
 
 import {iframeResizer} from 'iframe-resizer';
+
+export function init(element, options) {
+	document.addEventListener("DOMContentLoaded", function(event) {
+		var iframe = document.createElement('iframe');
+		iframe.frameBorder=0;
+		iframe.width="100%";
+		iframe.scrolling="no";
+		iframe.onload=function() {iframeResizer(this);};
+		iframe.innerHTML="Browser does not support iframes";
+		iframe.src="/_synthy?actions=queryActions&queryVar=query";
+		element.appendChild(iframe);
+	});
+}
